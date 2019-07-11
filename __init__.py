6c4d6a1df3f12be5e0536b7190d1cb73aefee4b5
hania wattar <haniawattar@gmail.com>
2019-07-11 11:48:15 +0200

    add cli

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019  Projektpraktikum Python.
+# SPDX-License-Identifier: Apache-2.0
+
+"""Bezier curve converter
+"""
+
+__version__ = '0.0.0'
---
+++ cli.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019  Projektpraktikum Python.
+# SPDX-License-Identifier: Apache-2.0
+
+"""Command line interface
+"""
+
+
+def cli():
+    """Setuptools entrypoint
+    """
+    print('This is the cli entrypoint')
